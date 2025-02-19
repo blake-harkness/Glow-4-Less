cd25a85e06b5d36bbe0433ce0602e267fb6e0ced
Blake Harkness <blake.ac.harkness@gmail.com>
2025-02-19 20:58:21 +1300

    possible fix for static issues

--- makeup_replacer/vite.config.ts
+++ makeup_replacer/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,4 +12,17 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  base: '/',
 })
